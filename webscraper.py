0f89c761e7c789e862ea21c05fe29c4fc6718887
funny0619 <54945640+funny0619@users.noreply.github.com>
2019-12-27 17:37:59 +0800

    added ability to read and write a txt file
    reads list of url with space in between. writes data with line in between

--- webscraper.py
+++ webscraper.py
@@ -1,25 +1,37 @@
 from urllib.request import urlopen
-from urllib.request import Request
 from bs4 import BeautifulSoup as soup
 import urllib.request
 
-my_url = 'https://lihkg.com/thread/1797/page/1'
 
 class AppURLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0"
-opener = AppURLopener()
-page_html = opener.open(my_url).read()
-page_soup = soup(page_html, "html.parser", from_encoding="gb18030")
 
-def fyan8_parser():
-    for i in page_soup.select(".nrct"):
-        print (i.get_text().strip())
-def wikipedia_parser():
-    for i in page_soup.select('p'):
-        print(i.get_text().strip())
-def lihkg_parser():
-    containers = page_soup.find_all("div", class_ = "_3jxQCFWg9LDtkSkIVLzQ8L")
-    print(containers)
-    for i in page_soup.select("div._2cNsJna0_hV8tdMj3X6_gJ"):
-        print(i.get_text().strip())
-#lihkg parser does not work
+
+opener = AppURLopener()
+
+
+def parser(my_url):
+    plaintext = ''
+    page_html = opener.open(my_url).read()
+    page_soup = soup(page_html, "html.parser", from_encoding="gb18030")
+    if "wikipedia" in my_url:
+        for i in page_soup.select('p'):
+            plaintext = i.get_text().strip() + plaintext
+    else:
+        for i in page_soup.select(".nrct"):
+            plaintext = i.get_text().strip() + plaintext
+    return plaintext
+
+
+openFile = open("url_list.txt", "r") #reads a list of urls to scrape
+dataFile = open("traning_data.txt", "x") #creates a file to store data from url
+dataFile.close()
+dataFile = open("traning_data.txt", "a")
+my_url_list = openFile.read().split()
+for url in my_url_list:
+    my_url = url
+    dataFile.write(parser(my_url) + '\n\n')
+    
+openFile.close()
+dataFile.close()
+
