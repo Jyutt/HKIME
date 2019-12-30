f1479acedfe43a4d4f6f79d60ee6e03850c6dff6
Ethan Kiang <chocopuff298@gmail.com>
2019-12-30 15:59:52 +0800

    bi-gram class added as well as prob of single word p(w)

--- distribution.py
+++ distribution.py
@@ -13,7 +13,9 @@ class Distribution():
     """
     def __init__(self):
         self.counter = {}
+        self.single_counter = {}
         self.ngram_count = 0
+        self.singles_count = 0
 
     def add_occurence(self, n_gram, count=1):
         assert len(n_gram) == 3
@@ -25,6 +27,13 @@ class Distribution():
             self.ngram_count += 1
         else:
             self.counter[n_gram[:2]] += count
+
+        for w in n_gram:
+            if w not in self.single_counter:
+                self.single_counter[w] = count
+            else:
+                self.single_counter[w] += count
+        self.singles_count += count * 3
 
     def posterior(self, w, prior):
         """
@@ -40,6 +49,16 @@ class Distribution():
         else:
             return 0
 
+    def prob(self, w):
+        """
+        return:
+            P(w)
+        """
+        if w in self.single_counter:
+            return self.single_counter[w] / self.singles_count
+        else:
+            return 0
+
     def randomized(self):
         """
         Returns a generator that yields, in a random order, the
@@ -51,3 +70,31 @@ class Distribution():
         for p in priors:
             total = sum(self.counter[p].values())
             yield p, total, self.counter[p]
+
+
+class BiGramDistribution(Distribution):
+    """
+    FOR QUICK PROTOTYPING / TESTING purposes only
+    We will be using trigrams for the language model.
+    BiGramDistribution is only for testing out the Viterbi
+    algorithm implementation in graph.py before generalizing
+    to trigrams
+    """
+
+    def add_occurence(self, bi_gram, count=1):
+        assert len(bi_gram) == 2
+
+        if bi_gram[0] not in self.counter:
+            self.counter[bi_gram[0]] = {}
+        if bi_gram[1] not in self.counter[bi_gram[0]]:
+            self.counter[bi_gram[0]][bi_gram[1]] = count
+            self.ngram_count += 1
+        else:
+            self.counter[bi_gram[0]][bi_gram[1]] += count
+
+        for w in bi_gram:
+            if w not in self.single_counter:
+                self.single_counter[w] = count
+            else:
+                self.single_counter[w] += count
+        self.singles_count += count * 2
